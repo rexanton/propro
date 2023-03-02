ada1d5b53edcded5548197fbfbd2ddfe6ee72316
rexanton <antonyrex17032003@gmail.com~>
2023-03-02 17:22:52 +0530

    final

--- po1.java
+++ po1.java
@@ -2,8 +2,11 @@ class po1
 {
     public static void main(String ar[])
     {
-        System.out.println("Hii");
-        System.out.println("something");
-        
+        po1 p = new po1();
+        po1 p1=new po1();
+        p=null;
+        p=p1;
+        System.out.println("1 :"+p);
+        System.out.println("1 :"+p1);
     }
 }
